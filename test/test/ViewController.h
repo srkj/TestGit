27ba3db0ee571254cd878fc42259326f94f3d1bd
dlz15607 <379790257@qq.com>
2016-12-16 11:07:28 +0800

    Add files via upload

---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by EterNa1ove、 on 16/3/3.
+//  Copyright © 2016年 Monster.Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/test/ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by EterNa1ove、 on 16/3/3.
+//  Copyright © 2016年 Monster.Z. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "NSString+DES.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    NSString *des = @"11212121";
+    
+    NSString *string = [des stringWithStrToDES];
+    NSLog(@"---  %@",string);
+    
+    NSString *string1 = [string stringWithDESToStr];
+    NSLog(@"===  %@",string1);
+    
+    
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
